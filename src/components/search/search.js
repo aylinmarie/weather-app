4cc172f28dac5c8f53517d427bd333056e8b11ee
Aylin McGinnis <amcginnis@rsglab.com>
2019-04-17 11:15:40 -0400

    Refactor api fetch with componentDidMount.

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import './search.scss';
+import React from "react";
+import "./search.scss";
 
-import { Button, Form, FormGroup, Dropdown } from 'react-bootstrap';
-import { Animated } from 'react-animated-css';
-import { faRing } from '@fortawesome/pro-light-svg-icons';
+import { Button, Form, FormGroup } from "react-bootstrap";
+import { Animated } from "react-animated-css";
 
 class Search extends React.Component {
   constructor(props) {
     super(props);
-    this.getCountries();
+    this.state = {
+      country: [],
+    };
   }
-  getCountries = async e => {
-    const api_country = await fetch(`https://restcountries.eu/rest/v2/all`);
-    const response_country = await api_country.json();
 
-    for (let i = 0; i < response_country.length; i++) {
-      const country = response_country[i].name;
-      return country;
-    }
-  };
+  componentDidMount () {
+    fetch(`https://restcountries.eu/rest/v2/all`)
+    .then(results => {
+      return results.json();
+    }).then(data => {
+      let country = data.map((item) => {
+        return( 
+        <p>{item.name}</p>
+      )
+    })
+    this.setState({country: country});
+    console.log("country", this.state.country);
+    })
+  }
+
   render() {
     return (
       <div className="amd-search">
@@ -31,18 +39,7 @@ class Search extends React.Component {
                 name="city"
                 placeholder="city"
               />
-              <Form.Control
-                className="amd-field-input"
-                type="text"
-                name="country"
-                placeholder="country"
-              />
             </FormGroup>
-            {/* <Form.Group controlId="exampleForm.ControlSelect1">
-              <Form.Control as="select">
-                <option>1</option>
-              </Form.Control>
-            </Form.Group> */}
             <Button type="submit" className="amd-search-button">
               Get Weather
             </Button>
