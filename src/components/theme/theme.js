8c0d6c45a802fc4c5398943f3d981f982ce691e0
Aylin McGinnis <amcginnis@rsglab.com>
2019-04-17 14:56:59 -0400

    Set function for toggle

--- src/components/theme/theme.js
+++ src/components/theme/theme.js
@@ -7,13 +7,26 @@ import { ButtonToolbar, ToggleButtonGroup, ToggleButton } from "react-bootstrap"
 class Theme extends React.Component {
   constructor(props) {
     super(props);
+    this.handleChange = this.handleChange.bind(this);
+
+    this.state = {
+      value: [1],
+    };
+  }
+  handleChange(value, event) {
+    this.setState({ value });
   }
   
   render() {
     return (
       <div className="amd-theme-toggle">
         <ButtonToolbar>
-            <ToggleButtonGroup type="radio" name="options" defaultValue={1}>
+            <ToggleButtonGroup 
+                type="radio" 
+                name="options" 
+                value={this.state.value}
+                onChange={this.handleChange}
+                defaultValue={1}>
             <ToggleButton value={1}>Day</ToggleButton>
             <ToggleButton value={2}>Night</ToggleButton>
             </ToggleButtonGroup>
